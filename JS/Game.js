d4198b2f51d07180a59bd01b2e769920f2ab39fc
Yehonatan <yehonatan349@gmail.com>
2022-09-13 12:30:29 +0300

    FIX: All function private.

--- JS/Game.js
+++ JS/Game.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//TODO: Handle fps
+
 import { Player } from './Player.js';
 import { Grid } from './Grid.js';
 import { PauseBtn } from './PauseBtn.js';
@@ -7,26 +9,30 @@ import { Stars } from './stars.js';
 
 export class Game {
     constructor(canvas) {
-        try { 
-            typeof canvas.getContext !== "function"
+        try {
+            typeof canvas.getContext !== 'function' || canvas.getContext('2d');
+            this.canvas = canvas;
+        } catch (err) {
+            console.log(err);
+            const canvas = document.createElement('canvas');
+            canvas.setAttribute("id", "canvas");
+            document.body.appendChild(canvas);
+            this.canvas = canvas;
         }
-        catch (err) {
-            throw "You must provide a canvas element";
-        }
-        this.canvas = canvas;
-        this.canvas.height = window.innerHeight - 4; //FIXME:
+
+        this.canvas.height = window.innerHeight - 4; //FIXME
         this.canvas.width = window.innerWidth;
         this.ctx = this.canvas.getContext('2d');
-        this.player = new Player(this.ctx, this.canvas.width / 2, this.canvas.height);
-        this.grid = new Grid(3, 3, 10, this.canvas, this.ctx)
-        this.pauseBtn = new PauseBtn(this.ctx);
-        this.stars = new Stars(this.canvas, this.ctx);
+        this.player = new Player(this.canvas.width / 2, this.canvas.height);
+        this.grid = new Grid(3, 3, 10, this.canvas, this.canvas.height, this.canvas.width)
+        this.pauseBtn = new PauseBtn();
+        this.stars = new Stars(this.canvas.height, this.canvas.width);
         this.play = true;
         this.BICHYOUNused = false;
         this.frames = 0;
 
         this.canvas.addEventListener('click', (event) => {
-            if (this.isInside(this.getMousePos(event), this.pauseBtn.x_pos, this.pauseBtn.width, this.pauseBtn.y_pos, this.pauseBtn.height)) {
+            if (this.#isInside(this.#getMousePos(event), this.pauseBtn.x_pos, this.pauseBtn.width, this.pauseBtn.y_pos, this.pauseBtn.height)) {
                 if (this.play) {
                     this.play = false;
                     this.pauseBtn.image.src = './img/play.jpg'
@@ -37,79 +43,74 @@ export class Game {
             }
         }, false);
     }
-    static BICHYOUN = 'יש לי בכיון';
+    static #BICHYOUN = 'יש לי בכיון';
 
-    updete() {
-        this.printScore();
-        this.printBICHYOUN();
-    };
+    #updete() {
+        this.#printScore();
+        this.#printBICHYOUN();
+    }
 
-    printScore() {
+    #printScore() {
         this.ctx.font = '20px Arial';
         this.ctx.fillStyle = 'white';
-        this.ctx.fillText('Score:' + this.player.score, this.canvas.width - this.getWidthOfText(`Score: ${this.player.score}`, '20px Arial'), 30);
-    };
+        this.ctx.fillText('Score:' + this.player.score, this.canvas.width - this.#getWidthOfText(`Score: ${this.player.score}`, '20px Arial'), 30);
+    }
 
-    printNewLevel() {
-        this.printMsg('Level Up!',
+    #printNewLevel() {
+        this.#printMsg('Level Up!',
             '80px Arial',
-            (this.canvas.width / 2) - this.getWidthOfText('Level Up!', '80px Arial') / 2,
+            (this.canvas.width / 2) - this.#getWidthOfText('Level Up!', '80px Arial') / 2,
             this.canvas.height / 2)
-    };
+    }
 
-    printBICHYOUN() {
+    #printBICHYOUN() {
         this.ctx.font = '20px Arial';
         this.ctx.fillStyle = 'white';
-        this.ctx.fillText(Game.BICHYOUN, this.canvas.width - this.getWidthOfText(Game.BICHYOUN, '20px Arial'), 60);
-    };
-
-    pauseGame(event) {
-        const mousePos = this.getMousePos(event);
-        if (this.isInside(mousePos, this.pauseBtn.x_pos, this.pauseBtn.width, this.pauseBtn.y_pos, this.pauseBtn.height)) {
-            this.play = false;
-        }
+        this.ctx.fillText(Game.#BICHYOUN, this.canvas.width - this.#getWidthOfText(Game.#BICHYOUN, '20px Arial'), 60);
     }
 
     start() {
         this.#animate();
-    };
+    }
 
     #animate() {
         if (this.player.lose) {
-            this.printMsg('You Lose!',
+            this.#printMsg('You Lose!',
                 '80px Arial',
-                (this.canvas.width / 2) - this.getWidthOfText('You Lose!', '80px Arial') / 2,
+                (this.canvas.width / 2) - this.#getWidthOfText('You Lose!', '80px Arial') / 2,
                 this.canvas.height / 2);
 
-            this.printMsg('Play Again?',
+            this.#printMsg('Play Again?',
                 '40px Arial',
-                (this.canvas.width / 2) - this.getWidthOfText('Play Again?', '40px Arial') / 2,
-                this.canvas.height / 2 + this.getHeightOfText('You Lose!', '80px Arial'));
+                (this.canvas.width / 2) - this.#getWidthOfText('Play Again?', '40px Arial') / 2,
+                this.canvas.height / 2 + this.#getHeightOfText('You Lose!', '80px Arial'));
 
             this.canvas.addEventListener('click', (event) => {
-                this.bichyoun(event);
-                this.playAgain(event);
+                this.#bichyoun(event);
+                this.#playAgain(event);
             }, false);
         }
         else if (this.play) {
             this.ctx.clearRect(0, 0, innerWidth, innerHeight);
-            this.checkEnemyHit();
-            this.isLevelUp();
-            this.checkIfShotOutOfScreen();
-            this.isTouchingSpaceship();
-            this.updete();
-            this.player.update();
-            this.grid.update();
-            this.stars.update();
+            this.#checkEnemyHit();
+            this.#isLevelUp();
+            this.#checkIfShotOutOfScreen();
+            this.#isTouchingSpaceship();
+            this.#updete();
+            this.player.draw(this.ctx);
+            this.grid.draw(this.ctx);
+            this.stars.draw(this.ctx);
         }
-        this.pauseBtn.update();
+        if (!this.player.lose) {
+            this.pauseBtn.draw(this.ctx);
+        }
         requestAnimationFrame(() => this.#animate());
-    };
+    }
 
-    isLevelUp() {
+    #isLevelUp() {
         if (this.grid.enemys.length === 0) {
             if (this.frames != 100) {
-                this.printNewLevel();
+                this.#printNewLevel();
                 this.frames++;
             } else if (this.frames === 100) {
                 this.grid.rows += 1;
@@ -123,58 +124,59 @@ export class Game {
                 this.frames = 0;
             }
         }
-    };
+    }
 
-    bichyoun(event) {
-        const mousePos = this.getMousePos(event);
-        const BICHYOUNwidth = this.getWidthOfText(Game.BICHYOUN, '20px Arial');
-        const BICHYOUNheight = this.getHeightOfText(Game.BICHYOUN, '20px Arial') + this.getHeightOfText(`Score: ${this.player.score}`, '20px Arial');
+    #bichyoun(event) {
+        const mousePos = this.#getMousePos(event);
+        const BICHYOUNwidth = this.#getWidthOfText(Game.#BICHYOUN, '20px Arial');
+        const BICHYOUNheight = this.#getHeightOfText(Game.#BICHYOUN, '20px Arial') + this.#getHeightOfText(`Score: ${this.player.score}`, '20px Arial');
 
-        if (this.isInside(mousePos,
+        if (this.#isInside(mousePos,
             this.canvas.width - BICHYOUNwidth,
             this.canvas.width - BICHYOUNwidth + BICHYOUNwidth,
             BICHYOUNheight,
             BICHYOUNheight
         ) && !this.BICHYOUNused) {
             this.BICHYOUNused = true;
-            Game.BICHYOUN = 'בזבזת את הבכיון'
+            Game.#BICHYOUN = 'בזבזת את הבכיון'
             this.player.lose = false;
+            this.player.shots = [];
             this.grid.enemys = [];
             this.grid.shots = [];
             this.grid.createGrid();
         };
-    };
+    }
 
-    getWidthOfText(txt, font) {
+    #getWidthOfText(txt, font) {
         this.ctx.font = font;
         const { width } = this.ctx.measureText(txt);
         return width;
-    };
+    }
 
-    getHeightOfText(text, fontSize) {
+    #getHeightOfText(text, fontSize) {
         this.ctx.font = fontSize;
         this.ctx.fillText(text, -100, -100);
         const metrics = this.ctx.measureText(text);
         const textHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
         return textHeight;
-    };
+    }
 
-    getMousePos(event) {
+    #getMousePos(event) {
         const rect = this.canvas.getBoundingClientRect();
         return {
             x: event.clientX - rect.left,
             y: event.clientY - rect.top
         };
-    };
+    }
 
-    isInside(MousePos, x_pos, width, y_pos, height) {
+    #isInside(MousePos, x_pos, width, y_pos, height) {
         return MousePos.x > x_pos &&
             MousePos.x < x_pos + width &&
             MousePos.y > y_pos &&
             MousePos.y < y_pos + height
-    };
+    }
 
-    checkEnemyHit() {
+    #checkEnemyHit() {
         this.player.shots.forEach((shot, i) => {
             this.grid.enemys.forEach((enemy, j) => {
                 if (shot.x_pos - shot.radius >= enemy.x_pos &&
@@ -189,19 +191,17 @@ export class Game {
             }
             )
         })
-    };
+    }
 
-    checkIfShotOutOfScreen() {
+    #checkIfShotOutOfScreen() {
         this.player.shots.forEach((shot) => {
             if (shot.y_pos + shot.radius <= 0) {
                 this.player.shots.splice(shot, 1)
-            } else {
-                shot.update();
-            }
+            } 
         })
     }
 
-    isTouchingSpaceship() {
+    #isTouchingSpaceship() {
         this.grid.enemys.forEach((enemy) => {
             if (this.player.x_pos + this.player.width >= enemy.x_pos &&
                 this.player.x_pos + this.player.width / 2 <= enemy.x_pos + enemy.width &&
@@ -220,25 +220,26 @@ export class Game {
                 }
             })
         })
-    };
+    }
 
-    playAgain(event) {
-        // this.canvas.removeEventListener('click', (event) => {this.bichyoun(event);this.playAgain(event);}, false); //TODO: how to removeEventListener with arrow function 
-        const mousePos = this.getMousePos(event);
-        if (this.isInside(mousePos,
-            (this.canvas.width / 2) - this.getWidthOfText('Play Again?', '40px Arial') / 2,
-            this.getWidthOfText('Play Again?', '40px Arial'),
-            this.canvas.height / 2 + this.getHeightOfText('Play Again?', '40px Arial'),
-            this.getHeightOfText('You Lose!', '80px Arial'))
+    #playAgain(event) {
+        // this.canvas.removeEventListener('click', (event) => {this.bichyoun(event);this.#playAgain(event);}, false); //TODO: how to removeEventListener with arrow function 
+        const mousePos = this.#getMousePos(event);
+        if (this.#isInside(mousePos,
+            (this.canvas.width / 2) - this.#getWidthOfText('Play Again?', '40px Arial') / 2,
+            this.#getWidthOfText('Play Again?', '40px Arial'),
+            this.canvas.height / 2 + this.#getHeightOfText('Play Again?', '40px Arial'),
+            this.#getHeightOfText('You Lose!', '80px Arial'))
         ) {
-            Game.BICHYOUN = 'יש לי בכיון ';
+            Game.#BICHYOUN = 'יש לי בכיון ';
             this.grid = new Grid(3, 3, 10, this.canvas, this.ctx)
             this.player = new Player(this.ctx, this.canvas.width / 2, this.canvas.height);
         }
-    };
-    printMsg(msg, fontSize, x_pos, y_pos) {
+    }
+
+    #printMsg(msg, fontSize, x_pos, y_pos) {
         this.ctx.font = fontSize;
         this.ctx.fillStyle = 'green';
         this.ctx.fillText(msg, x_pos, y_pos)
-    };
-};
+    }
+}
